d69d12e33bdf2c137c7f8dd23d7fb84af464b57d
actonDev <chris.actondev@gmail.com>
2020-03-28 04:14:31 +0100

    region item: beat division tick (midi text events)

--- ActonDev_Region Item - Insert.lua
+++ ActonDev_Region Item - Insert.lua
@@ -4,9 +4,10 @@ local Log = require('utils.log')
 local Item = require('utils.item')
 local Common = require('utils.common')
 local Store = require('utils.store')
+local Midi = require('utils.midi')
 
 Common.undoBeginBlock()
--- Common.preventUIRefresh(1)
+Common.preventUIRefresh(1)
 
 -- Log.isdebug = true
 Store.storeCursorPosition()
@@ -24,17 +25,27 @@ local envelope = reaper.GetTakeEnvelope(take, 0)
 reaper.InsertEnvelopePoint(envelope, 0, 1, 0, 0, false)
 reaper.InsertEnvelopePoint(envelope, Item.getInfo(item, Item.PARAM.LENGTH), -1, 0, 0, false)
 
-local retval,name  = reaper.GetUserInputs("Insert Item Title", 1, "Region Item Title", "")
-if retval then
+local opts = {[1] = {title = 'Region item name',
+                     default = 'region'},
+              [2] = {title = 'Tick division (eg 16th notes)',
+                     default = '16'}}
+local res = Common.getUserInput("Insert region items",opts)
+
+if res then
+    local name = res[1]
+    local division = res[2]
     Item.setActiveTakeInfoString(item, Item.TAKE_PARAM.STR_NAME, name)
+
+    local qnLength = Midi.qnLength(item)
+    local qnInterval = 4/division
+    local marks = qnLength/qnInterval
+    for i=0,marks-1 do
+        Midi.insertTextEvent(item,i*qnInterval, tostring(i))
+    end
+
 end
-
--- TODO? add 16th note midi text events? useful
-
 
 
 Store.restoreCursorPosition()
--- Common.preventUIRefresh(-1)
+Common.preventUIRefresh(-1)
 Common.undoEndBlock("actondev/Region Item: Insert")
--- Item properties: Toggle show media item/take properties
--- Common.cmd(41589)
--- utils/common.lua
+++ utils/common.lua
@@ -1,11 +1,11 @@
 local module = {}
-
+local Log = require('utils.log')
 function module.cmd(id)
-	if type(id) == "string" then
-		reaper.Main_OnCommand(reaper.NamedCommandLookup(id),0)
-	else
-		reaper.Main_OnCommand(id, 0)
-	end
+    if type(id) == "string" then
+        reaper.Main_OnCommand(reaper.NamedCommandLookup(id), 0)
+    else
+        reaper.Main_OnCommand(id, 0)
+    end
 end
 
 function module.undoBeginBlock()
@@ -18,11 +18,61 @@ function module.undoEndBlock(undoMsg)
 end
 
 function module.preventUIRefresh(prevent_count)
-    reaper.PreventUIRefresh( prevent_count)
+    reaper.PreventUIRefresh(prevent_count)
 end
 
 function module.updateArrange()
     reaper.UpdateArrange()
+end
+
+function module.moveWindowToMouse()
+    module.cmd("_BR_MOVE_WINDOW_TO_MOUSE_H_M_V_B")
+end
+
+local function split(str, pat)
+    local t = {}-- NOTE: use {n = 0} in Lua-5.0
+    local fpat = "(.-)" .. pat
+    local last_end = 1
+    local s, e, cap = str:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(t, cap)
+        end
+        last_end = e + 1
+        s, e, cap = str:find(fpat, last_end)
+    end
+    if last_end <= #str then
+        cap = str:sub(last_end)
+        table.insert(t, cap)
+    end
+    return t
+end
+
+--[[
+    example opts
+    {[1] = {title = 'Region item name',
+                     default = 'region'},
+              [2] = {title = 'Tick division (eg 16th notes)',
+                     default = '16'}}
+]]
+function module.getUserInput(title, opts)
+    local titlesCsv = ""
+    local valuesCsv = ""
+    for _, entry in ipairs(opts) do
+        titlesCsv = titlesCsv .. entry['title'] .. ","
+        valuesCsv = valuesCsv .. entry['default'] .. ","
+    end
+    
+    titlesCsv = titlesCsv:sub(1, #titlesCsv - 1)
+    valuesCsv = valuesCsv:sub(1, #valuesCsv - 1)
+    
+    local retval, valuesCsv = reaper.GetUserInputs(title, 2, titlesCsv, valuesCsv)
+    if not retval then
+        return nil
+    end
+
+    local valuesTable = split(valuesCsv,"[,]+")
+    return valuesTable
 end
 
 return module
