ea1015aa76b0c9ec753184eb15eedafb5f7bcc04
actonDev <chris.actondev@gmail.com>
2020-03-30 15:28:14 +0200

    WIP gui

--- src/actondev_Command Palette.lua
+++ src/actondev_Command Palette.lua
@@ -2,6 +2,7 @@ package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;
 
 local Log = require('utils.log')
 local Actions = require('utils.actions')
+local Gui = require('utils.gui')
 local Common = require('utils.common')
 
 Log.isdebug = true
@@ -15,3 +16,34 @@ if #res > 1 then
 end
 
 Log.debug(Log.dump(res))
+
+function init()
+    gfx.init("actondev/Command Palette", 400, 100)
+    Common.moveWindowToMouse()
+end
+
+function mainloop()
+	if gfx.mouse_cap == 0 then
+		if not hasClicked then
+			-- initDraw()
+		else
+			-- release()
+		end
+	elseif gfx.mouse_cap == 1 then
+		-- left click
+		hasClicked = true
+		clickDraw()
+    end
+    gfx.x =0
+    gfx.y=0
+    Gui.drawString("hi there")
+	gfx.update()
+	local c=gfx.getchar()
+	if c~=Gui.CHAR.ESCAPE and c~=Gui.CHAR.EXIT then
+		reaper.defer(mainloop)
+	end
+end
+
+
+init()
+mainloop()
