542ea0555048a411df3b0349ee29aeecc049217d
actonDev <chris.actondev@gmail.com>
2020-04-09 02:21:16 +0200

    observe: active take offset :)

--- src/aod/reaper/observe.lua
+++ src/aod/reaper/observe.lua
@@ -20,6 +20,28 @@ module.itemSelection = function()
     return self
 end
 
+module.activeTakeOffset = function()
+    local self = {item = nil, offset = nil}
+    function self.init()
+        self.item = Item.firstSelected()
+        self.offset = Item.getActiveTakeInfo(self.item, Item.TAKE_PARAM.START_OFFSET)
+        return self
+    end
+    function self.changed()
+        local item = Item.firstSelected()
+        local offset = Item.getActiveTakeInfo(item, Item.TAKE_PARAM.START_OFFSET)
+        if item == self.item and offset == self.offset then
+            return false
+        else
+            self.item = item
+            self.offset = offset
+            return true
+        end
+    end
+
+    return self
+end
+
 module.dummy = function()
     local self = {}
     function self.init()
@@ -66,7 +88,8 @@ end
 function module.getAll()
     return {
         {name = "On item selection change", handler = module.itemSelection()},
-        {name = "On cursor position change", handler = module.cursorPosition()}
+        {name = "On cursor position change", handler = module.cursorPosition()},
+        {name = "On active take offset change", handler = module.activeTakeOffset()}
     }
 end
 
