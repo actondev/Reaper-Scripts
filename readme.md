3c97919cf76b282afc4237093325a6bc92896d68
actonDev <chris.actondev@gmail.com>
2020-04-09 18:41:24 +0200

    updated readme

--- readme.md
+++ readme.md
@@ -1,8 +1,28 @@
-## ReaperScripts v1
+# actondev/Reaper-Scripts v1
 
+See the [changelog](./CHANGELOG.md)
+
+## Region Items
+
+### Propagate changes
+Propagate changes to similarly named region items
+![Propagate](./img/regions-propagate-manipulate.gif)
+
+### Pull changes
+Pull changes from a previous region item (that contains this subregion)
+![Propagate](./img/regions-pull.gif)
+
+## Command Palette
+![Command Palette](./img/command-palette-demo2.gif)
+
+## GUI mini framework
+GUI scripts are easy (I hope..) to modify, see [GUI v1](./src/aod/gui)
+
+## Midi item > item arrangements
+TBD
 
 ## TODOs
- - [ ] insert region item: should disable the 'loop source' option
+ - [ ] insert region item: should disable the 'loop source' option ?
  - [ ] recheck the midi item arrangement
    - item copy bug? if so, post at reaper forum
  - [ ] item arrangement 2 midi
--- src/actondev_Region Item - Select.lua
+++ src/actondev_Region Item - Select.lua
@@ -1,19 +1,27 @@
 package.path = debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" .. package.path
 
-local Log = require('aod.utils.log')
-local Common = require('aod.reaper.common')
-local RegionItems = require('aod.region_items')
-local Store = require('aod.reaper.store')
+local Log = require("aod.utils.log")
+local Common = require("aod.reaper.common")
+local RegionItems = require("aod.region_items")
+local Store = require("aod.reaper.store")
+local Item = require("aod.reaper.item")
 
-Log.isdebug = true
-
-local regionItem = reaper.GetSelectedMediaItem(0, 0)
+local selected = {}
 
 Common.undoBeginBlock()
 Common.preventUIRefresh(1)
 Store.storeTimeSelection()
 
-RegionItems.select(regionItem)
+for _, item in ipairs(Item.selected()) do
+    RegionItems.select(item)
+    for _, currSel in ipairs(Item.selected()) do
+        selected[#selected+1] = currSel
+    end
+end
+
+for _, item in ipairs(selected) do
+    Item.setSelected(item, true)
+end
 
 Store.restoreTimeSelection()
 Common.preventUIRefresh(-1)
