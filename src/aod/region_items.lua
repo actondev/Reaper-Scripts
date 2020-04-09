92a573d8bde0b2e7f4dbe561c732ea8a4438f2bf
actonDev <chris.actondev@gmail.com>
2020-04-09 02:05:38 +0200

    region items: fixed paste bug.. with a hack

--- src/aod/region_items.lua
+++ src/aod/region_items.lua
@@ -1,11 +1,11 @@
-local Common = require('aod.reaper.common')
-local Log = require('aod.utils.log')
-local Track = require('aod.reaper.track')
-local Item = require('aod.reaper.item')
-local EditCursor = require('aod.reaper.edit_cursor')
-local TimeSelection = require('aod.reaper.time_selection')
-local ItemManipulation = require('aod.item_manipulation')
-local Parse = require('aod.utils.parse')
+local Common = require("aod.reaper.common")
+local Log = require("aod.utils.log")
+local Track = require("aod.reaper.track")
+local Item = require("aod.reaper.item")
+local EditCursor = require("aod.reaper.edit_cursor")
+local TimeSelection = require("aod.reaper.time_selection")
+local ItemManipulation = require("aod.item_manipulation")
+local Parse = require("aod.utils.parse")
 
 local module = {}
 
@@ -19,7 +19,7 @@ local SELECT_MODE = {
 local function getSelectMode(track)
     local trackName = Track.name(track)
     local firstChar = string.sub(trackName, 1, 1)
-    
+
     if firstChar == "*" then
         return SELECT_MODE.ALL
     elseif firstChar == ">" then
@@ -59,28 +59,28 @@ function module.select(regionItem, startOffset, length)
     end
     Item.unselectAll()
     Item.setSelected(regionItem, true)
-    
+
     local track = Track.fromItem(regionItem)
     local tstart, tend = Item.startEnd(regionItem)
     -- or 0: could be an empty item
     local regionItemOffset = Item.getActiveTakeInfo(regionItem, Item.TAKE_PARAM.START_OFFSET) or 0
-    
+
     if length == nil then
         length = tend - tstart
     end
     if startOffset == nil then
         startOffset = regionItemOffset
     end
-    
+
     local subregionStart = tstart + startOffset - regionItemOffset
     local subregionEnd = subregionStart + length
-    
+
     -- have to see which part I should update: intersection of this region against the subregion
     local intersectionStart = math.max(tstart, subregionStart)
     local intersectionEnd = math.min(tend, subregionEnd)
     if intersectionStart < intersectionEnd then
         TimeSelection.set(intersectionStart, intersectionEnd)
-        
+
         local selMode = getSelectMode(track)
         if selMode == SELECT_MODE.SIBLINGS then
             selectSiblings(track)
@@ -92,7 +92,7 @@ function module.select(regionItem, startOffset, length)
         Item.selectInTimeSelectionAcrossSelectedTracks()
     else
         -- no common time between this region and the passed subregion
-        end
+    end
     Track.selectOnly(track)
 end
 
@@ -100,92 +100,105 @@ local function shouldPropagate(source, target)
     if source == target then
         return false
     end
-    
+
     local sourceType = Item.type(source)
     local targetType = Item.type(target)
-    
+
     if sourceType ~= targetType then
         return false
     end
-    
+
     if sourceType == Item.TYPE.EMPTY then
         return Item.notes(source) == Item.notes(target)
     end
     local sourceName = Item.activeTakeName(source)
     local targetName = Item.activeTakeName(target)
-    
+
     return sourceName == targetName
 end
 
 function module.propagateFromTo(sourceRegionItem, targetRegionItem)
+    if not shouldPropagate(sourceRegionItem, targetRegionItem) then
+        Log.warn(
+            "Should not propagate from item with notes " ..
+                Item.notes(sourceRegionItem) .. " to " .. Item.notes(targetRegionItem)
+        )
+        return
+    end
     local sourceStart, sourceEnd = Item.startEnd(sourceRegionItem)
 
-    -- TODO.. better copy always? there are problems concerning the first item
-    -- if shouldCopy then
-        module.select(sourceRegionItem)
-        -- unselecting region item: we don't copy it
-        Item.setSelected(sourceRegionItem, false)
-        -- copying only the area of the source region
-        TimeSelection.set(sourceStart, sourceEnd)
-        Item.copySelectedArea()
-    -- end
-    
+    module.select(sourceRegionItem)
+    -- unselecting region item: we don't copy it
+    Item.setSelected(sourceRegionItem, false)
+    -- copying only the area of the source region
+    TimeSelection.set(sourceStart, sourceEnd)
+    Item.copySelectedArea()
+    local copiedCount = Item.selectedCount()
+
     -- we need the first selected item to later select this item's track when pasting:
     -- in order for the items to be pasted in the correct track
     local firstSelItem = Item.firstSelected()
     local sourceRegionOffset = Item.getActiveTakeInfo(sourceRegionItem, Item.TAKE_PARAM.START_OFFSET)
-    
+
     -- Note: sourceRegion could be a subregion and targetRegion the full region.
     -- In that case we want to update only the corresponding subregion inside the full region
-    if shouldPropagate(sourceRegionItem, targetRegionItem) then
-        -- Log.debug("target " .. Item.notes(targetRegion))
-        module.clear(targetRegionItem, sourceRegionOffset, sourceEnd - sourceStart)
-        local tstart, tend = Item.startEnd(targetRegionItem)
-        
-        if firstSelItem then
-            -- need to "touch" the first track that I copy the items from
-            -- if not they get pasted in wrong places
-            local firstTrack = Track.fromItem(firstSelItem)
-            Track.selectOnly(firstTrack)
-            EditCursor.setPosition(tstart)
+    module.clear(targetRegionItem, sourceRegionOffset, sourceEnd - sourceStart)
+    local tstart, tend = Item.startEnd(targetRegionItem)
+
+    if firstSelItem then
+        -- need to "touch" the first track that I copy the items from
+        -- if not they get pasted in wrong places
+        local firstTrack = Track.fromItem(firstSelItem)
+        Track.selectOnly(firstTrack)
+        EditCursor.setPosition(tstart)
+        reaper.UpdateTimeline()
+        Common.updateArrange()
+        Item.paste()
+        while Item.selectedCount() ~= copiedCount do
+            -- Note: bugs happen..
+            -- TODO fucking bug here, make a bug report
+            -- Log.warn("buggy paste on item with notes ", Item.notes(targetRegionItem))
             Item.paste()
         end
-        
-        local targetRegionOffset = Item.getActiveTakeInfo(targetRegionItem, Item.TAKE_PARAM.START_OFFSET)
-        Item.adjustInfoSelected(Item.PARAM.POSITION, sourceRegionOffset - targetRegionOffset)
-        
-        -- trimming pasted items to this region time range
-        Item.splitSelected(tstart)
-        Item.splitSelected(tend)
-        Item.deleteSelectedOutsideOfRange(tstart, tend)
-        
-        -- adjusting pitch
-        local targetPitch = Item.getActiveTakeInfo(targetRegionItem, Item.TAKE_PARAM.PITCH)
-        Item.adjustActiveTakeInfoSelected(Item.TAKE_PARAM.PITCH, targetPitch)
-        
-        -- manipulating target region
-        local targetRegionNotes = Item.notes(targetRegionItem)
-        local manipulationOpts = Parse.parsedTaggedJson(targetRegionNotes, ItemManipulation.TAG_V1)
-        ItemManipulation.manipulateSelected(manipulationOpts)
+    else
+        Log.warn("no first item??")
     end
+
+    local targetRegionOffset = Item.getActiveTakeInfo(targetRegionItem, Item.TAKE_PARAM.START_OFFSET)
+    Item.adjustInfoSelected(Item.PARAM.POSITION, sourceRegionOffset - targetRegionOffset)
+    -- trimming pasted items to this region time range
+    Item.splitSelected(tstart)
+    Item.splitSelected(tend)
+    Item.deleteSelectedOutsideOfRange(tstart, tend)
+
+    -- adjusting pitch
+    local targetPitch = Item.getActiveTakeInfo(targetRegionItem, Item.TAKE_PARAM.PITCH)
+    Item.adjustActiveTakeInfoSelected(Item.TAKE_PARAM.PITCH, targetPitch)
+
+    -- manipulating target region
+    local targetRegionNotes = Item.notes(targetRegionItem)
+    local manipulationOpts = Parse.parsedTaggedJson(targetRegionNotes, ItemManipulation.TAG_V1)
+    ItemManipulation.manipulateSelected(manipulationOpts)
 end
 
 function module.propagate(regionItem)
     if regionItem == nil then
         return
     end
-    
+
     local track = Track.fromItem(regionItem)
     Track.selectOnly(track)
     Item.selectAllInSelectedTrack()
-    
+
     Item.setSelected(regionItem, false)
     local otherRegionItems = Item.selected()
-    
+
     for _i, targetRegion in ipairs(otherRegionItems) do
-        module.propagateFromTo(regionItem, targetRegion)
+        if shouldPropagate(regionItem, targetRegion) then
+            module.propagateFromTo(regionItem, targetRegion)
+        end
     end
-    
+
     Track.selectOnly(track)
     Item.unselectAll()
     Item.setSelected(regionItem, true)
@@ -211,7 +224,7 @@ function module.pull(regionItem)
     local track = Track.fromItem(regionItem)
     Track.selectOnly(track)
     Item.selectAllInSelectedTrack()
-    
+
     Item.setSelected(regionItem, false)
     local otherRegionItems = Item.selected()
 
@@ -222,7 +235,7 @@ function module.pull(regionItem)
             break
         end
     end
-    
+
     Track.selectOnly(track)
     Item.unselectAll()
     Item.setSelected(regionItem, true)
@@ -241,6 +254,8 @@ function module.clear(regionItem, startOffset, length)
     Item.setSelected(regionItem, false)
     Item.splitSelectedTimeSelection()
     Item.deleteSelected()
+    -- note: should I select it again? seems to be ok
+    Item.setSelected(regionItem, true)
 end
 
 return module
